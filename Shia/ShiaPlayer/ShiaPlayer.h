820e84c40e9f6318583e5bff0dc967780aef99c3
anthonyprograms <anthonygreen274@icloud.com>
2017-10-08 11:45:57 -0700

    Created ShiaPlayer framework

---
+++ Shia/ShiaPlayer/ShiaPlayer.h
@@ -0,0 +1,19 @@
+//
+//  ShiaPlayer.h
+//  ShiaPlayer
+//
+//  Created by Anthony Williams on 10/8/17.
+//  Copyright © 2017 Anthony Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ShiaPlayer.
+FOUNDATION_EXPORT double ShiaPlayerVersionNumber;
+
+//! Project version string for ShiaPlayer.
+FOUNDATION_EXPORT const unsigned char ShiaPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ShiaPlayer/PublicHeader.h>
+
+
